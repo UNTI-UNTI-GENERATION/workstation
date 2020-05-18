efceebd47554546f2660befb8b2c868e3c665593
Yuki Nishii <dubhemerakphecdamegrez@gmail.com>
2020-05-18 23:17:27 +0900

    move json file & add router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const     app = express();
-const    PORT = 80;
-const    HOST = '0.0.0.0';
+const  router = require('./router');
+const    port = 80;
+const    host = '0.0.0.0';
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+app.use('/', router);
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(port, host);
+console.log(`Running on http://${host}:${host}`);
